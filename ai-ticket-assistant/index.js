a5c875dc21ed31ca22770e2506fc9af64bcbb0aa
sujalshelke-git <sujalshelke18@gmail.com>
2025-07-18 17:52:14 +0530

    new commit

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -1,14 +1,32 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import userRoutes from "./routes/user.js"
+import { serve } from "inngest/express";
+import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use("/api/auth", userRoutes)
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
 
 mongoose
   .connect(process.env.MONGO_URI)
