6ea44ab3448f0bc525982344bd6c7fad5673c28f
sujalshelke-git <sujalshelke18@gmail.com>
2025-07-26 12:46:18 +0530

    frontend url in cors

--- ai-ticket-assistant/index.js
+++ ai-ticket-assistant/index.js
@@ -14,7 +14,10 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://full-stack-ai-agent.vercel.app/"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
