11528b45a954caf92c36271ecffcd43756fd3ffe
sujalshelke-git <sujalshelke18@gmail.com>
2025-07-29 12:24:51 +0530

    .

--- ai-ticket-frontend/vite.config.js
+++ ai-ticket-frontend/vite.config.js
@@ -1,23 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   build: {
-
     outDir: 'dist',
-
   },
-
   plugins: [react()],
-
   resolve: {
-
     alias: {
-
       '@': path.resolve(__dirname, 'src'),
-
     },
-
   },
-  
 })
